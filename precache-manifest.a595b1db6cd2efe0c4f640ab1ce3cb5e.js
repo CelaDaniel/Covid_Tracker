d847437d40127a5b7dfa63f4573563d482bbc758
Daniel Cela <danielcela111@gmail.com>
2022-06-04 00:45:09 +0200

    Updates

---
+++ precache-manifest.a595b1db6cd2efe0c4f640ab1ce3cb5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4673ac87499dbb2b2ba3",
+    "url": "/DanielCela111/COVID_Tracker/static/css/main.cf94bc24.chunk.css"
+  },
+  {
+    "revision": "4673ac87499dbb2b2ba3",
+    "url": "/DanielCela111/COVID_Tracker/static/js/main.4673ac87.chunk.js"
+  },
+  {
+    "revision": "540e49fc9eacff39157c",
+    "url": "/DanielCela111/COVID_Tracker/static/js/1.540e49fc.chunk.js"
+  },
+  {
+    "revision": "7dd57049f637da76644e",
+    "url": "/DanielCela111/COVID_Tracker/static/css/2.fcd9db35.chunk.css"
+  },
+  {
+    "revision": "7dd57049f637da76644e",
+    "url": "/DanielCela111/COVID_Tracker/static/js/2.7dd57049.chunk.js"
+  },
+  {
+    "revision": "6e8bc7ea66f3f551c618",
+    "url": "/DanielCela111/COVID_Tracker/static/js/runtime~main.6e8bc7ea.js"
+  },
+  {
+    "revision": "c04940fa00b5efde46373659ff33537a",
+    "url": "/DanielCela111/COVID_Tracker/index.html"
+  }
+];
